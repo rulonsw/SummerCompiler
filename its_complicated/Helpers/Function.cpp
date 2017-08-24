4bd1a069f83363bc5e893e2e0c14d334e90db404
Rulon Wood <rulonsdangerwoodiv@gmail.com>
2017-08-24 13:53:16 -0600

    Add Function Class Declarations
    Work on procedures/functions will now commence in the appropriate feature branch.

---
+++ its_complicated/Helpers/Function.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Rulon Wood on 8/24/17.
+//
+
+#include "Function.h"
---
+++ its_complicated/Helpers/Function.h
@@ -0,0 +1,14 @@
+//
+// Created by Rulon Wood on 8/24/17.
+//
+
+#ifndef SUMMERCOMPILER_FUNCTION_H
+#define SUMMERCOMPILER_FUNCTION_H
+
+
+class Function {
+
+};
+
+
+#endif //SUMMERCOMPILER_FUNCTION_H
